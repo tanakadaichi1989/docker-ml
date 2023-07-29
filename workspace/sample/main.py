394468aeff2d66ed194fafba855f85ae23150733
tanakadaichi_1989 <tanakadaichi_1989@icloud.com>
2023-07-29 23:45:39 +0900

    Add sample directory

---
+++ workspace/sample/main.py
@@ -0,0 +1,9 @@
+from sample import Sample
+
+print("main")
+
+sample = Sample()
+
+sample.execute()
+sample.load()
+sample.write()
---
+++ workspace/sample/sample.py
@@ -0,0 +1,13 @@
+class Sample:
+    
+    def __init__(self):
+        pass
+    
+    def execute(self):
+        print("executed execute function in sample.py")
+        
+    def load(self):
+        print("executed load function in sample.py")
+        
+    def write(self):
+        print("executed write function in sample.py")
